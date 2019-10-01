1e25f17afb4ad5c887a78fcca94259bc1d07b793
vale75020 <valentinosuma@hotmail.com>
2019-10-01 18:25:59 +0200

    todo_items fetch

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+// set dynamic classes
+setClasses() {
+  let classes = {
+    todo: true,
+    'is-complete':  this.todo.completed
+  }
+  return classes;
+}
+
+onToggle(todo) {
+  console.log('toggle');
+  todo.completed = !todo.completed;
+}
+
+onDelete(todo) {
+  console.log('delete');
+}
+
+}
